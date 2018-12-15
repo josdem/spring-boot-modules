14aa59bf362a4f6b16eaf2eca4b460995fe7dafc
josdem <joseluis.delacruz@gmail.com>
2018-12-15 12:30:56 -0500

    Moving to the right packages

--- src/main/java/com/jos/dem/springboot/module/controller/ModuleController.java
+++ src/main/java/com/jos/dem/springboot/module/controller/ModuleController.java
@@ -1,10 +1,16 @@
-package com.jos.dem.springboot.module.application.controller;
+package com.jos.dem.springboot.module.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jos.dem.springboot.module.library.service.MessageService;
 
 @RestController
 public class ModuleController {
+
+  @Autowired
+  private MessageService messageService;
 
   @GetMapping("/")
   public String index(){
