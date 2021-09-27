0b5b4058d787ac1bde8ca68f98f9c5dce3753524
josdem <joseluis.delacruz@gmail.com>
2021-09-27 07:43:21 -0500

    Formatting code

--- application/src/main/java/com/jos/dem/springboot/module/ModuleApplication.java
+++ application/src/main/java/com/jos/dem/springboot/module/ModuleApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModuleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModuleApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ModuleApplication.class, args);
+  }
 }
-
